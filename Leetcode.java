53bd978e4c91c7835739a06a8b600744e197fb24
Master-mohit <mohitmansigupta9098@gmail.com>
2025-02-08 23:51:14 +0530

    217

--- Leetcode.java
+++ Leetcode.java
@@ -89,3 +89,18 @@ class Solution {
        return count;
     }
 }
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i =0;i<nums.length;i++){
+
+            if(set.contains(nums[i]))
+            return true;
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
