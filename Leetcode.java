d7c3a0b1c8639ddfd19a15a86a5dfe2cf021fade
Master-mohit <mohitmansigupta9098@gmail.com>
2025-02-17 23:36:29 +0530

    181

--- Leetcode.java
+++ Leetcode.java
@@ -145,3 +145,11 @@ class Solution {
         return false;
     }
 }
+
+
+# Write your MySQL query statement below
+SELECT e.name AS Employee
+FROM Employee e
+INNER JOIN Employee m
+    ON e.managerId = m.id
+WHERE e.salary > m.salary;
