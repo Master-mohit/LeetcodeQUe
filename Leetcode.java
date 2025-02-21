bc801315cd60f15eb1b90a5478ba6e04555d4dc3
Master-mohit <mohitmansigupta9098@gmail.com>
2025-02-22 03:31:04 +0530

    1360

--- Leetcode.java
+++ Leetcode.java
@@ -153,3 +153,15 @@ FROM Employee e
 INNER JOIN Employee m
     ON e.managerId = m.id
 WHERE e.salary > m.salary;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+class Solution {
+    public int daysBetweenDates(String date1, String date2) {
+        LocalDate d1 = LocalDate.parse(date1);
+        LocalDate d2 = LocalDate.parse(date2);
+
+        long daysBetween = Math.abs(ChronoUnit.DAYS.between(d1,d2));
+        return (int) daysBetween;
+    }
+}
